b22e0e11cd25ea791bd1aadc35fe64369c3f91be
Giovanni Ranieri <gioran2017@gmail.com>
2023-05-01 13:56:04 +0200

    :+1: change

--- App.tsx
+++ App.tsx
@@ -1,15 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-interface Human {
-  name: string;
-  age: number
-}
-
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello worl</Text>
+      <Text>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
