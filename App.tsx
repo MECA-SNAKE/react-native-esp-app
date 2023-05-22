0080756c679922ae656e69e3fc6774366590cc60
Yeri M <lea@grieder.org>
2023-05-22 14:32:22 +0200

    m

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 
 //rajouter un user friendly interface pour faire bouger le snake a droite et à gauche
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Slider from '@react-native-community/slider'
-import { StyleSheet, Text, TouchableOpacity, View, Alert, ScrollView} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
 
 interface Props { }
@@ -14,243 +15,238 @@ const App: React.FC<Props> = () => {
   const [showGame, setShowGame] = useState(false)
 
   const [run, setRun] = useState(false)
-  const [WLsliderValue, setWLSliderValue] = useState(0.5)
-  const [AmplsliderValue, setAmplSliderValue] = useState(30)
-  const [FreqsliderValue, setFreqSliderValue] = useState(0.5)
-  const [isConcertinaPressed, setIsConcertinaPressed] = useState(false);
-  const [isUndulatedPressed, setIsUndulatedPressed] = useState(false);
+  const [WLsliderValue, setWLSliderValue] = useState(1)
+  const [AmplsliderValue, setAmplSliderValue] = useState(40)
+  const [FreqsliderValue, setFreqSliderValue] = useState(2)
+  const [SpeedSliderValue, setSpeedSliderValue] = useState(1)
+  const [OffsetSliderValue, setOffsetSliderValue] = useState(0)
+  const [isConcertinaPressed, setIsConcertinaPressed] = useState(false)
+  const [isUndulatedPressed, setIsUndulatedPressed] = useState(false)
   const [isBackwardsPressed, setIsBackwards] = useState(false)
+  const [isForwardPressed, setIsForward] = useState(false)
   const [isInchwormPressed, setIsInchworm] = useState(false)
-  const [isForwardPressed, setIsForwardPressed] = useState(false)
-  const [isLeftPressed, setIsLeftPressed] = useState(false)
-  const [isRightPressed, setIsRightPressed] = useState(false)
-
 
   function receiveRequests() {
-      console.log('Im checking')
-      axios
-        .get('http://192.168.34.121/sensor')
-        .then((response) => {
-          if (response.data === 1) {
-            setRun(false);
-            createButtonAlert('DANGER! Hole ahead! Snake stopped...');
-          } else {
-            console.log('no hole found');
-          }
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
+    console.log('Im checking')
+    axios
+      .get('http://192.168.236.121/sensor')
+      .then((response) => {
+        if (response.data === 1) {
+          setRun(false);
+          createButtonAlert('DANGER! Hole ahead! Snake stopped...');
+        } else {
+          console.log('no hole found');
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   }
-
-  useEffect(() => {
-    if(run){
-      const intervalId = setInterval(receiveRequests, 1000);
-      return () => {
-        clearInterval(intervalId);
-      };
-    }
-    else console.log("No need to check, snake is stopped")
-  }, [run]);
-
-  function sendRequests(val: string, root: string){
-    axios.post('http://192.168.34.121/' + root, {
-      "value": val
+  /*
+    useEffect(() => {
+      if (run) {
+        const intervalId = setInterval(receiveRequests, 1000);
+        return () => {
+          clearInterval(intervalId);
+        };
+      }
+      else console.log("No need to check, snake is stopped")
+    }, [run]);
+  */
+  function sendRequests(key: string, val: string, root: string) {
+    /*axios.post('http://192.168.236.121/' + root, {
+      key: val
+    }, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
     }).then((response) => {
       console.log(response);
     }, (error) => {
       console.log(error);
-    });
+    });*/
 
   }
 
   const handleButtonStartPress = () => {
-    if(isConcertinaPressed || isUndulatedPressed || isInchwormPressed){
-      if(isConcertinaPressed) sendRequests("0", "motion")
-      else if(isUndulatedPressed) sendRequests("1", "motion")
-      else if(isInchwormPressed) sendRequests("2", "motion")
+    if ((isConcertinaPressed || isUndulatedPressed || isInchwormPressed)
+      && isBackwardsPressed || isForwardPressed) {
+      setRun(true)
+      sendRequests("value", "1", "mode") //start
       console.log('START')
       setShowGame(true)
-      setRun(false)
     }
-    else createButtonAlert("Please select a motion before starting")
+    else createButtonAlert("Please select a motion and a direction before starting!")
   }
 
-  const handleButtonStopPress = () => {
+  const handleButtonStopPress = () => { //TODO: HANDLE STOP ON SHOWGAME!
     setRun(false)
-    setIsForwardPressed(false)
-    setIsBackwards(false)
-    setIsLeftPressed(false)
-    setIsRightPressed(false)
-    sendRequests("0", "mode")
+    sendRequests("value", "0", "mode")
     console.log('STOP')
   }
 
   const handleButtonConcertina = () => {
-    if(!run){
+    if (isConcertinaPressed) return
+    else {
       setIsConcertinaPressed(!isConcertinaPressed);
       setIsUndulatedPressed(false)
       setIsInchworm(false)
       console.log('CONCERTINA BUTTON ENABLED')
+      sendRequests("value", "0", "motion")
     }
-    else console.log("Ignoring concertina button pressed...")
   }
 
   const handleButtonUndulated = () => {
-    if(!run){
+    if (isUndulatedPressed) return
+    else {
       setIsUndulatedPressed(!isUndulatedPressed);
       setIsConcertinaPressed(false)
       setIsInchworm(false)
       console.log('UNDULATED BUTTON ENABLED')
+      sendRequests("value", "1", "motion")
     }
-    else console.log("Ignoring undulated button pressed...")
   }
 
   const handleButtonInchworm = () => {
-    if(!run) {
+    if (isInchwormPressed) return
+    else {
       setIsInchworm(!isInchwormPressed)
       setIsConcertinaPressed(false)
-      setWLSliderValue(5)
-      setFreqSliderValue(5)
-      setAmplSliderValue(30)
+      setWLSliderValue(1)
+      setFreqSliderValue(2)
+      setAmplSliderValue(40)
       setIsUndulatedPressed(false)
       console.log('INCHWORM BUTTON ENABLED')
+      sendRequests("value", "2", "motion")
     }
-    else console.log("Ignoring inchworm button pressed...")
   }
 
   const handleWLChange = (value: number) => {
-    if(isConcertinaPressed || isUndulatedPressed){
+    if (isUndulatedPressed) {
       console.log('new WL value updated')
       setWLSliderValue(value / 10);
-      sendRequests(String(value), "paramsWL")
+      sendRequests("wl", String(value / 10), "params")
     }
-    else console.log("Ignoring WaveLength value change")
   }
 
   const handleFreqChange = (value: number) => {
-    if(isConcertinaPressed || isUndulatedPressed){
+    if (isUndulatedPressed) {
       console.log('new Freq value updated')
       setFreqSliderValue(value / 10);
-      sendRequests(String(value/10), "paramsFreq")
+      sendRequests("freq", String(value / 10), "params")
     }
-    else console.log("Ignoring Frequency value change")
   }
 
   const handleAmplChange = (value: number) => {
-    if(isConcertinaPressed || isUndulatedPressed){
+    if (isUndulatedPressed) {
       console.log('new Ampl value updated')
       setAmplSliderValue(value);
-      sendRequests(String(value), "paramsAmpl")
+      sendRequests("amp", String(value), "params")
     }
-    else console.log("Ignoring Amplitude value change")
+  }
+
+  const handleSpeedInchwormChange = (value: number) => {
+    if (isInchwormPressed) {
+      console.log('new Speed value updated')
+      setSpeedSliderValue(value / 10);
+      sendRequests("speed", String(value / 10), "params")
+    }
+  }
+
+  const handleOffsetChange = (value: number) => {
+    if (isUndulatedPressed) {
+      console.log('new offset value updated ' + String(value / 10))
+      setOffsetSliderValue(value / 10);
+      sendRequests("offset", String(value / 10), "params")
+    }
   }
 
   const handleButtonReset = () => {
-    sendRequests("0", "reset")
+    sendRequests("value", "0", "reset")
     setRun(false)
     setIsBackwards(false)
+    setIsForward(false)
     setIsConcertinaPressed(false)
     setIsUndulatedPressed(false)
     setIsInchworm(false)
-    setIsRightPressed(false)
-    setIsLeftPressed(false)
-    setIsForwardPressed(false)
-    setAmplSliderValue(0)
-    setFreqSliderValue(0)
-    setWLSliderValue(0)
+    setAmplSliderValue(40)
+    setFreqSliderValue(2)
+    setWLSliderValue(1)
+    setSpeedSliderValue(1)
+    setOffsetSliderValue(0)
     console.log("RESET")
-    if(showGame){
+    if (showGame) {
       setShowGame(false)
     }
   }
 
   const handleBackwards = () => {
-    if(!run){
-      console.log("BACKWARDS")
-      setIsBackwards(true)
-      setIsForwardPressed(false)
-      setRun(true)
-      sendRequests("0", "direction")
-    }
-    else console.log("Ignoring backwards button pressed...")
+    console.log("BACKWARDS")
+    setIsBackwards(true)
+    setIsForward(false)
+    sendRequests("value", "0", "direction")
   }
-  
+
   const handleForward = () => {
-    if(!run){
-      console.log("FORWARDS")
-      setIsBackwards(false)
-      setIsForwardPressed(true)
-      setRun(true)
-      sendRequests("1", "direction")
-    }
-    else console.log("Ignoring forwards button pressed...")
+    console.log("FORWARDS")
+    setIsBackwards(false)
+    setIsForward(true)
+    sendRequests("value", "1", "direction")
   }
-
-
-  const createButtonAlert = (message: string) =>{
-    Alert.alert("ERROR", message, [
-      {text: 'OK', onPress: () => console.log('OK Pressed')},
-    ]);
-  }
-
-
 
   const handleLeft = () => {
-    if(run){
-      setIsRightPressed(false)
-      setIsLeftPressed(true);
-      sendRequests("2", "direction")
-      console.log("LEFT")
-    }
-    else console.log("ignoring left, snake is stopped")
+    console.log("LEFT")
+    setOffsetSliderValue((OffsetSliderValue / 10) - 0.1);
+    sendRequests("offset", String((OffsetSliderValue / 10) - 0.1), "params")
+    console.log("offset: " + OffsetSliderValue / 10)
   }
 
   const handleRight = () => {
-    if(run){
-      setIsLeftPressed(false)
-      sendRequests("3", "direction")
-      setIsRightPressed(true)
-      console.log("RIGHT")
-    }
-    else console.log("ignoring right, snake is stopped")
+    console.log("RIGHT")
+    setOffsetSliderValue((OffsetSliderValue / 10) + 0.1);
+    sendRequests("offset", String((OffsetSliderValue / 10) + 0.1), "params")
+    console.log("offset: " + OffsetSliderValue / 10)
   }
 
 
-  if(showGame) {
-    return(
+  const createButtonAlert = (message: string) => {
+    Alert.alert("ERROR", message, [
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ]);
+  }
+
+  if (showGame) {
+    return (
       <View style={styles.container1}>
         <TouchableOpacity style={styles.button} onPress={handleButtonReset}>
           <Text style={styles.buttonText}>Reset</Text>
         </TouchableOpacity>
 
-          <TouchableOpacity style={[styles.roundButton, isForwardPressed && styles.pressedButton]} onPress={handleForward}>
-            <Icon name="up" size={24} color="#FFFFFF" />
-          </TouchableOpacity>
+        <TouchableOpacity style={styles.roundButton} onPress={handleForward}>
+          <Icon name="up" size={24} color="#FFFFFF" />
+        </TouchableOpacity>
 
-        <View style = {styles.arrowButtonContainer}>
+        <View style={styles.arrowButtonContainer}>
 
-          <TouchableOpacity style={[styles.roundButton, isLeftPressed && styles.pressedButton]} onPress={handleLeft}>
+          <TouchableOpacity style={styles.roundButton} onPress={handleLeft}>
             <Icon name="left" size={24} color="#FFFFFF" />
           </TouchableOpacity>
 
-          <TouchableOpacity style={[styles.roundButton, !run && styles.pressedButton]} onPress={handleButtonStopPress}>
+          <TouchableOpacity style={styles.roundButton} onPress={handleButtonStopPress}>
             <Text style={styles.buttonText}>STOP</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={[styles.roundButton, isRightPressed && styles.pressedButton]} onPress={handleRight}>
+          <TouchableOpacity style={styles.roundButton} onPress={handleRight}>
             <Icon name="right" size={24} color="#FFFFFF" />
           </TouchableOpacity>
 
         </View>
 
-      <TouchableOpacity style={[styles.roundButton, isBackwardsPressed && styles.pressedButton]} onPress={handleBackwards}>
-            <Icon name="down" size={24} color="#FFFFFF" />
-          </TouchableOpacity>
+        <TouchableOpacity style={styles.roundButton} onPress={handleBackwards}>
+          <Icon name="down" size={24} color="#FFFFFF" />
+        </TouchableOpacity>
 
       </View>
-
-  
 
     )
   }
@@ -258,16 +254,15 @@ const App: React.FC<Props> = () => {
 
   return (
 
-    <ScrollView>
-      <View style={[styles.container1, { marginTop: 80, marginBottom: 80, marginLeft: 30, marginRight: 30 }]}>
+    <View style={styles.container1}>
 
       <TouchableOpacity style={styles.button} onPress={handleButtonReset}>
-          <Text style={styles.buttonText}>Reset</Text>
+        <Text style={styles.buttonText}>Reset</Text>
       </TouchableOpacity>
 
       <Text style={styles.titleMode}>MODE</Text>
-      
-      <View style = {styles.containerButtons}>
+
+      <View style={styles.containerButtons}>
         <TouchableOpacity style={[styles.button, run && styles.pressedButton]} onPress={handleButtonStartPress}>
           <Text style={styles.buttonText}>Start</Text>
         </TouchableOpacity>
@@ -279,7 +274,7 @@ const App: React.FC<Props> = () => {
 
       <Text style={styles.titleMode}>MOTION</Text>
 
-      <View style = {styles.containerButtons}>
+      <View style={styles.containerButtons}>
         <TouchableOpacity style={[styles.button, isConcertinaPressed && styles.pressedButton]} onPress={handleButtonConcertina}>
           <Text style={styles.buttonMotion}>Concertina</Text>
         </TouchableOpacity>
@@ -295,63 +290,94 @@ const App: React.FC<Props> = () => {
       </View>
 
       <Text style={styles.titleMode}>DIRECTION</Text>
-        <View style = {styles.containerButtons}>
-          <TouchableOpacity style={[styles.button, isBackwardsPressed && styles.pressedButton]} onPress={handleBackwards}>
-            <Text style={styles.buttonMotion}>Backwards</Text>
-          </TouchableOpacity>
+      <View style={styles.containerButtons}>
+        <TouchableOpacity style={[styles.button, isBackwardsPressed && styles.pressedButton]} onPress={handleBackwards}>
+          <Text style={styles.buttonMotion}>Backwards</Text>
+        </TouchableOpacity>
 
-          <TouchableOpacity style={[styles.button, !isBackwardsPressed && styles.pressedButton]} onPress={handleForward}>
-            <Text style={styles.buttonMotion}>Forward</Text>
-          </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, isForwardPressed && styles.pressedButton]} onPress={handleForward}>
+          <Text style={styles.buttonMotion}>Forward</Text>
+        </TouchableOpacity>
 
       </View>
 
       <Text style={styles.titleMode}>PARAMETERS</Text>
+      <Text style={styles.subTitle}>Set Offset</Text>
+
+      <Slider
+        value={OffsetSliderValue}
+        onValueChange={(value) => setOffsetSliderValue(value / 10)}
+        onSlidingComplete={(value) => handleOffsetChange(value)}
+        minimumValue={-100}
+        maximumValue={100}
+        step={1}
+        style={{ width: 200, height: 40 }}
+        disabled={(isInchwormPressed || isConcertinaPressed) ||
+          (!isConcertinaPressed && !isUndulatedPressed && !isInchwormPressed)}
+      />
+      <Text>Value: {OffsetSliderValue}</Text>
 
       <Text style={styles.subTitle}>Set WaveLength</Text>
 
       <Slider
-        style={{width: 200, height: 40}}
-        minimumValue={5}
-        maximumValue={40}
-        step={1}
+        style={{ width: 200, height: 40 }}
+        minimumValue={10}
+        maximumValue={30}
+        step={5}
         value={WLsliderValue}
-        disabled={run || isInchwormPressed || !(isConcertinaPressed || isUndulatedPressed)}
-        onValueChange={(value) => setWLSliderValue(value/10)}
+        disabled={isInchwormPressed || isConcertinaPressed ||
+          (!isConcertinaPressed && !isUndulatedPressed && !isInchwormPressed)}
+        onValueChange={(value) => setWLSliderValue(value / 10)}
         onSlidingComplete={(value) => handleWLChange(value)}
-        />
-        <Text>Value: {WLsliderValue}</Text>
+      />
+      <Text>Value: {WLsliderValue}</Text>
 
-        
+
       <Text style={styles.subTitle}>Set Amplitude</Text>
 
       <Slider
-        style={{width: 200, height: 40}}
-        minimumValue={30}
+        style={{ width: 200, height: 40 }}
+        minimumValue={20}
         maximumValue={70}
         step={1}
-        disabled={run || isInchwormPressed || !(isConcertinaPressed || isUndulatedPressed)}
+        disabled={isInchwormPressed || isConcertinaPressed ||
+          (!isConcertinaPressed && !isUndulatedPressed && !isInchwormPressed)}
         value={AmplsliderValue}
         onSlidingComplete={(value) => handleAmplChange(value)}
         onValueChange={(value) => setAmplSliderValue(value)}
-        />
-        <Text>Value: {AmplsliderValue}</Text>
+      />
+      <Text>Value: {AmplsliderValue}</Text>
 
-        <Text style={styles.subTitle}>Set Frequency</Text>
+      <Text style={styles.subTitle}>Set Frequency</Text>
 
-        <Slider
+      <Slider
         value={FreqsliderValue}
-        onValueChange={(value) => setFreqSliderValue(value/10)}
+        onValueChange={(value) => setFreqSliderValue(value / 10)}
         onSlidingComplete={(value) => handleFreqChange(value)}
         minimumValue={5}
         maximumValue={40}
         step={1}
-        style={{width: 200, height: 40}}
-        disabled={run || isInchwormPressed || !(isConcertinaPressed || isUndulatedPressed)}
-        />
-        <Text>Value: {FreqsliderValue}</Text>
+        style={{ width: 200, height: 40 }}
+        disabled={isInchwormPressed || isConcertinaPressed ||
+          (!isConcertinaPressed && !isUndulatedPressed && !isInchwormPressed)}
+      />
+      <Text>Value: {FreqsliderValue}</Text>
+
+      <Text style={styles.subTitle}>Set Speed Inchworm</Text>
+
+      <Slider
+        value={SpeedSliderValue}
+        onValueChange={(value) => setSpeedSliderValue(value / 10)}
+        onSlidingComplete={(value) => handleSpeedInchwormChange(value)}
+        minimumValue={2}
+        maximumValue={40}
+        step={1}
+        style={{ width: 200, height: 40 }}
+        disabled={isUndulatedPressed || isConcertinaPressed ||
+          (!isConcertinaPressed && !isUndulatedPressed && !isInchwormPressed)}
+      />
+      <Text>Value: {SpeedSliderValue}</Text>
     </View>
-  </ScrollView>
   );
 };
 
@@ -374,7 +400,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
 
   },
-  titleMode:{
+  titleMode: {
     fontSize: 30,
     fontWeight: 'bold',
     marginBottom: 16,
